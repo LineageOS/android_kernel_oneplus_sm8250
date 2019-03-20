8c19f509fbe8262daf2f8f7b3e04b252a23ba594
dianlujitao <dianlujitao@lineageos.org>
2019-03-20 21:43:45 +0100

    proc: Remove androidboot.selinux=permissive flag from /proc/cmdline
     * Oneplus bootloader is setting this when booting recovery
    
     * Inspired by I8925d3b6e102902e0e31fda25ffe568fcd37185a by
       dianlujitao <dianlujitao@lineageos.org>
    
    Change-Id: I8925d3b6e102902e0e31fda25ffe568fcd37185a

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,16 +3,42 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <asm/setup.h>
+
+static char new_command_line[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_puts(m, saved_command_line);
+	seq_puts(m, new_command_line);
 	seq_putc(m, '\n');
 	return 0;
 }
 
 static int __init proc_cmdline_init(void)
 {
+	char *offset_addr, *cmd = new_command_line;
+
+	strcpy(cmd, saved_command_line);
+
+	/*
+	 * Remove 'androidboot.selinux' flag from command line set
+	 * by bootloader when booting recovery
+	 */
+	offset_addr = strstr(cmd, "androidboot.selinux=permissive");
+	if (offset_addr) {
+		size_t i, len, offset;
+
+		len = strlen(cmd);
+		offset = offset_addr - cmd;
+
+		for (i = 1; i < (len - offset); i++) {
+			if (cmd[offset + i] == ' ')
+				break;
+		}
+
+		memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
+	}
+
 	proc_create_single("cmdline", 0, NULL, cmdline_proc_show);
 	return 0;
 }
