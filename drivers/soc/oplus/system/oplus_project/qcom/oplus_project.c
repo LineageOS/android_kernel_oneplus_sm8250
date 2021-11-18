8caa0de2864d298dd73eb5c769e606afb1f494dc
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-11-18 20:13:24 +0530

    soc: oplus_project: Fix pointer to integer cast
    ../drivers/soc/oplus/system/oplus_project/qcom/oplus_project.c:501:12: warning: cast to smaller integer type 'uint32_t' (aka 'unsigned int') from 'void *' [-Wvoid-pointer-to-int-cast]
            switch(Ptr2UINT32(p)) {
                   ^~~~~~~~~~~~~
    ../drivers/soc/oplus/system/oplus_project/qcom/oplus_project.c:26:26: note: expanded from macro 'Ptr2UINT32'
    #define Ptr2UINT32(p)    (uint32_t)(p)
                             ^~~~~~~~~~~~~
    
    Change-Id: I7f9af8e6d765400dc3187dc80e959b49357a7eaa

--- drivers/soc/oplus/system/oplus_project/qcom/oplus_project.c
+++ drivers/soc/oplus/system/oplus_project/qcom/oplus_project.c
@@ -23,7 +23,7 @@
 #define SMEM_PROJECT 135
 
 #define UINT2Ptr(n) (uint32_t *)(n)
-#define Ptr2UINT32(p) (uint32_t)(p)
+#define Ptr2UINT32(p) (uintptr_t)(p)
 
 #define PROJECT_VERSION (0x1)
 #define PCB_VERSION (0x2)
