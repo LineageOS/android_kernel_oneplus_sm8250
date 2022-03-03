252c56d5a8f599523f4f607ae0d86564506acbfa
Tejas Prajapati <quic_tpraja@quicinc.com>
2022-03-03 15:27:31 +0530

    msm: camera-oplus: memmgr: update correct length in bufq
    In a corner case race condition to free the original
    ion buf allocated and new ion buffer with same fd but
    different size after freeing the origianl ion buf might
    result into mismatches in the real ion buf size assigned
    in bufq. To avoid this update length in bufq with
    local variable.
    
    Change-Id: I0c76f7124de0cf5389592595872e695e07fc24d7
    Signed-off-by: Tejas Prajapati <quic_tpraja@quicinc.com>

--- techpack/camera-oplus/drivers/cam_req_mgr/cam_mem_mgr.c
+++ techpack/camera-oplus/drivers/cam_req_mgr/cam_mem_mgr.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2019, 2022 The Linux Foundation. All rights reserved.
  */
 
 #include <linux/module.h>
@@ -665,7 +665,7 @@ int cam_mem_mgr_alloc_and_map(struct cam_mem_mgr_alloc_cmd *cmd)
 	tbl.bufq[idx].kmdvaddr = kvaddr;
 	tbl.bufq[idx].vaddr = hw_vaddr;
 	tbl.bufq[idx].dma_buf = dmabuf;
-	tbl.bufq[idx].len = cmd->len;
+	tbl.bufq[idx].len = len;
 	tbl.bufq[idx].num_hdl = cmd->num_hdl;
 	memcpy(tbl.bufq[idx].hdls, cmd->mmu_hdls,
 		sizeof(int32_t) * cmd->num_hdl);
