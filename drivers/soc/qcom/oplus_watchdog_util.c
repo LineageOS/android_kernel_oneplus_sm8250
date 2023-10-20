8751f7cd3640778741515712cf0926b10d8a0347
LuK1337 <priv.luk@gmail.com>
2023-10-20 10:03:18 +0200

    treewide: oplus: Address clang-17 build warnings
    Change-Id: Ieb6364a71148d1e51d62ade04c926dcbe7cf712e

--- drivers/soc/qcom/oplus_watchdog_util.c
+++ drivers/soc/qcom/oplus_watchdog_util.c
@@ -23,7 +23,7 @@ extern int cpu_idle_pc_state[NR_CPUS];
 unsigned int smp_call_any_cpu;
 unsigned long smp_call_many_cpumask;
 int recovery_tried;
-static oplus_print_utc_cnt = 0;
+static int oplus_print_utc_cnt = 0;
 
 static const char *recoverable_procs[] = { "SearchDaemon", "libsu.so",
 					   "NotificationObs" };
@@ -162,7 +162,7 @@ static void print_top10_irqs(void)
 	       o_irq_counter.all_irqs_delta);
 }
 
-void dump_cpu_online_mask()
+void dump_cpu_online_mask(void)
 {
 	static char alive_mask_buf[MASK_SIZE];
 	struct cpumask avail_mask;
@@ -190,7 +190,7 @@ void get_cpu_ping_mask(cpumask_t *pmask)
 	       o_irq_counter.all_irqs_delta);
 }
 
-void print_smp_call_cpu()
+void print_smp_call_cpu(void)
 {
 	printk(KERN_INFO "cpu of last smp_call_function_any: %d\n",
 	       smp_call_any_cpu);
@@ -258,7 +258,7 @@ int try_to_recover_pending(struct task_struct *w_task)
 	return 0;
 }
 
-void reset_recovery_tried()
+void reset_recovery_tried(void)
 {
 	recovery_tried = 0;
 }
