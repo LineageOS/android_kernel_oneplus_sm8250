8357ee00dfda9fe3ca1b1922961d6573b6161990
LuK1337 <priv.luk@gmail.com>
2025-10-19 14:20:57 +0200

    treewide: Remove OPLUS_FEATURE_POWERINFO_STANDBY
    Change-Id: I13b083db4ac85b51619ec84676057e74023031cc

--- net/oplus_nwpower/oplus_nwpower.c
+++ net/oplus_nwpower/oplus_nwpower.c
@@ -21,9 +21,6 @@
 #include <linux/version.h>
 #include <net/tcp.h>
 #include <net/oplus_nwpower.h>
-#ifdef OPLUS_FEATURE_POWERINFO_STANDBY
-#include <soc/oplus/oplus_wakelock_profiler.h>
-#endif /* OPLUS_FEATURE_POWERINFO_STANDBY */
 
 static void tcp_output_hook_work_callback(struct work_struct *work);
 static void tcp_input_hook_work_callback(struct work_struct *work);
@@ -723,9 +720,6 @@ static void tcp_input_hook_work_callback(struct work_struct *work) {
 	match_tcp_hook(&mdaci_tcp_input_list);
 	app_wakeup_monitor(&app_wakeup_monitor_list, false, true, tcp_input_list.pid, tcp_input_list.uid);
 	app_wakeup_monitor(&mdaci_app_wakeup_monitor_list, false, true, tcp_input_list.pid, tcp_input_list.uid);
-	#ifdef OPLUS_FEATURE_POWERINFO_STANDBY
-	wakeup_reasons_statics(IRQ_NAME_MODEM_IPA, WS_CNT_MODEM);
-	#endif /* OPLUS_FEATURE_POWERINFO_STANDBY */
 	if (tcp_input_list.is_ipv6) {
 		printk("[oplus_nwpower] IPAInputWakeup: [%ld,****], %d, %d, %d",
 			tcp_input_list.ipv6_addr1,
