cbd26732f428cce3381c409538fbfd8c839e152c
Michael Bestas <mkbestas@lineageos.org>
2026-01-02 11:46:48 +0200

    input: touchscreen: Drop unsupported devices
    Change-Id: I074a8dee8a10e9d7bf890aa4e46aa7c9e5612284

--- drivers/input/touchscreen/touch.c
+++ drivers/input/touchscreen/touch.c
@@ -14,7 +14,6 @@
 #include "oplus_touchscreen/touchpanel_common.h"
 #include <soc/oplus/system/oplus_project.h>
 #include <soc/oplus/device_info.h>
-#include "touch.h"
 
 #define MAX_LIMIT_DATA_LENGTH 100
 extern char *saved_command_line;
@@ -100,27 +99,6 @@ int tp_util_get_vendor(struct hw_resource *hw_res,
 		memcpy(panel_data->manufacture_info.version, "0x", 2);
 		panel_data->manufacture_info.version[2] = '\0';
 	}
-	if (prj_id == 19795) {
-		memcpy(panel_data->manufacture_info.version, "goodix_", 7);
-		panel_data->manufacture_info.version[7] = '\0';
-	}
-	if (prj_id == 19015 || prj_id == 19016) {
-		memcpy(panel_data->manufacture_info.version, "0xbd3180000", 11);
-		panel_data->manufacture_info.version[11] = '\0';
-	}
-	if (prj_id == 19125) {
-		memcpy(panel_data->manufacture_info.version, "0xbd2830000", 11);
-		panel_data->manufacture_info.version[11] = '\0';
-	}
-	if (prj_id == 20801) {
-		memcpy(panel_data->manufacture_info.version, "0x504000000", 11);
-		panel_data->manufacture_info.version[11] = '\0';
-	}
-	if (prj_id == 21623) {
-		memcpy(panel_data->manufacture_info.version, "focalt_",
-		       sizeof("focalt_"));
-		panel_data->manufacture_info.version[sizeof("focalt_")] = '\0';
-	}
 	if (g_tp_ext_prj_name) {
 		strncpy(panel_data->manufacture_info.version +
 				strlen(panel_data->manufacture_info.version),
@@ -145,65 +123,6 @@ int tp_util_get_vendor(struct hw_resource *hw_res,
 
 	panel_data->manufacture_info.fw_path = panel_data->fw_name;
 
-	if (prj_id == 20669 || prj_id == 20751) {
-		snprintf(panel_data->fw_name, MAX_FW_NAME_LENGTH,
-			 "tp/20669/FW_%s_%s.img", panel_data->chip_name,
-			 vendor);
-
-		if (panel_data->test_limit_name) {
-			snprintf(panel_data->test_limit_name,
-				 MAX_LIMIT_DATA_LENGTH,
-				 "tp/20669/LIMIT_%s_%s.img",
-				 panel_data->chip_name, vendor);
-		}
-		pr_info("panel_data->tp_type = %d\n", panel_data->tp_type);
-		if (panel_data->tp_type == TP_JDI) {
-			memcpy(panel_data->manufacture_info.version,
-			       "AA869_DS_NT_", 12);
-			panel_data->firmware_headfile.firmware_data =
-				FW_17951_NT36672C_JDI;
-			panel_data->firmware_headfile.firmware_size =
-				sizeof(FW_17951_NT36672C_JDI);
-		} else {
-			memcpy(panel_data->manufacture_info.version,
-			       "AA869_BOE_ILI_", 14);
-			panel_data->firmware_headfile.firmware_data =
-				FW_20669_ILI7807S;
-			panel_data->firmware_headfile.firmware_size =
-				sizeof(FW_20669_ILI7807S);
-		}
-	}
-	if (prj_id == 21027) {
-		strcpy(panel_data->manufacture_info.manufacture, "BOE");
-		memcpy(panel_data->manufacture_info.version, "BSFA26105", 9);
-		panel_data->firmware_headfile.firmware_data =
-			FW_21027_NT36523_BOE;
-		panel_data->firmware_headfile.firmware_size =
-			sizeof(FW_21027_NT36523_BOE);
-		snprintf(panel_data->fw_name, MAX_FW_NAME_LENGTH,
-			 "tp/%d/FW_%s_%s.bin", prj_id, panel_data->chip_name,
-			 vendor);
-
-		if (panel_data->test_limit_name) {
-			snprintf(panel_data->test_limit_name,
-				 MAX_LIMIT_DATA_LENGTH, "tp/%d/LIMIT_%s_%s.img",
-				 prj_id, panel_data->chip_name, vendor);
-		}
-	}
-	if (prj_id == 0x2065C) {
-		snprintf(panel_data->fw_name, MAX_FW_NAME_LENGTH,
-			 "tp/%s/FW_%s_%s.img", "2065C", panel_data->chip_name,
-			 vendor);
-
-		if (panel_data->test_limit_name) {
-			snprintf(panel_data->test_limit_name,
-				 MAX_LIMIT_DATA_LENGTH, "tp/%s/LIMIT_%s_%s.img",
-				 "2065C", panel_data->chip_name, vendor);
-		}
-		memcpy(panel_data->manufacture_info.version, "focalt_0000", 11);
-		panel_data->manufacture_info.fw_path = panel_data->fw_name;
-	}
-
 	pr_info("[TP]vendor:%s fw:%s limit:%s\n", vendor, panel_data->fw_name,
 		panel_data->test_limit_name == NULL ?
 			"NO Limit" :
@@ -219,53 +138,6 @@ EXPORT_SYMBOL(preconfig_power_control);
 
 int reconfig_power_control(struct touchpanel_data *ts)
 {
-	int ret = 0;
-	int prj_id = 0;
-	prj_id = get_project();
-
-	if ((prj_id == 20135 || prj_id == 20137 || prj_id == 20139 ||
-	     prj_id == 20235) &&
-	    !strstr(saved_command_line,
-		    "20135samsung_amb655xl08_1080_2400_cmd_dvt")) {
-		pr_err("[TP]pcb is old version, need to reconfig the regulator.\n");
-		if (!IS_ERR_OR_NULL(ts->hw_res.vdd_2v8)) {
-			regulator_put(ts->hw_res.vdd_2v8);
-			ts->hw_res.vdd_2v8 = NULL;
-		}
-		ts->hw_res.vdd_2v8 = regulator_get(ts->dev, "vdd_dvt_2v8");
-		if (IS_ERR_OR_NULL(ts->hw_res.vdd_2v8)) {
-			pr_err("[TP]Regulator vdd2v8 get failed, ret = %d\n",
-			       ret);
-		} else {
-			if (regulator_count_voltages(ts->hw_res.vdd_2v8) > 0) {
-				pr_err("[TP]set avdd voltage to %d uV\n",
-				       ts->hw_res.vdd_volt);
-				if (ts->hw_res.vdd_volt) {
-					ret = regulator_set_voltage(
-						ts->hw_res.vdd_2v8,
-						ts->hw_res.vdd_volt,
-						ts->hw_res.vdd_volt);
-				} else {
-					ret = regulator_set_voltage(
-						ts->hw_res.vdd_2v8, 3100000,
-						3100000);
-				}
-				if (ret) {
-					dev_err(ts->dev,
-						"Regulator set_vtg failed vdd rc = %d\n",
-						ret);
-				}
-				ret = regulator_set_load(ts->hw_res.vdd_2v8,
-							 200000);
-				if (ret < 0) {
-					dev_err(ts->dev,
-						"Failed to set vdd_2v8 mode(rc:%d)\n",
-						ret);
-				}
-			}
-		}
-	}
-
 	return 0;
 }
 EXPORT_SYMBOL(reconfig_power_control);
